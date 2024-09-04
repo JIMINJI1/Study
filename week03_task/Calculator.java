73900522ab3226312b1e6d5990beac378ba78e3c
JIMINJI1 <mj013031@gmail.com>
2024-09-04 17:57:04 +0900

    연산 클래스 추가 및 Calculator를 의존성 주입 Step3

--- week03_task/Calculator.java
+++ week03_task/Calculator.java
@@ -1,19 +1,33 @@
 package task.chap03_task;
 
-
 public class Calculator {
+    //Calculator 클래스의 생성자
+    private final AddOperation addOperation;  // AddOperation 클래스 타입의 addOperation 변수선언 AddOperation 객체를 참조 연산 수행 함
+    private final SubstractOperation substractOperation;
+    private final MultiplyOperation multiplyOperation;
+    private final DivideOperation divideOperation;
+
+    public Calculator(AddOperation addOperation,SubstractOperation substractOperation,MultiplyOperation multiplyOperation, DivideOperation divideOperation ) {
+        //this -> 현재 인스턴스(객체)
+        this.addOperation = addOperation;
+        this.substractOperation = substractOperation;
+        this.multiplyOperation = multiplyOperation;
+        this.divideOperation = divideOperation;
+
+    }
+
     public double calculate (String operator, int firstNumber, int secondNumber ) { // 매개변수로 값 받음
-        // 계산 결과 값
+        // 계산 결과 값 저장할 변수
         double answer = 0;
-        // 연산자 판별 if
+        // 연산자에 따라 연산함
         if(operator.equals("+")){
-            answer = firstNumber + secondNumber;
+            answer = addOperation.operate(firstNumber,secondNumber); //addOperation의 operate메서드 호출하여 연산함
         } else if (operator.equals("-")){
-            answer = firstNumber - secondNumber;
+            answer = substractOperation.operate(firstNumber,secondNumber);
         } else if (operator.equals("*")){
-            answer = firstNumber * secondNumber;
+            answer = multiplyOperation.operate(firstNumber,secondNumber);
         } else if (operator.equals("/")){ //몫
-            answer = firstNumber / secondNumber;
+            answer = divideOperation.operate(firstNumber,secondNumber);
         } else if (operator.equals("%")){ //나머지
             answer= firstNumber % secondNumber;
         }
--- week03_task/Main.java
+++ week03_task/Main.java
@@ -3,7 +3,8 @@ package task.chap03_task;
 public class Main {
     public static void main(String[] args) {
         // Calculator 객체 생성
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator(new AddOperation(), new SubstractOperation(),
+                new MultiplyOperation(), new DivideOperation());  // Calculator 객체를 생성하면서 네 개의 연산 객체를 주입
 
         //Calculator 클래스의 인스턴스인 calculator 객체에서 calculate 메서드를 실행
         // calculate (String operator, int firstNumber, int secondNumber )
